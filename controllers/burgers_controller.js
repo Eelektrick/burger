2e27e4a3da794c2395d573ac27f15080b0128794
Corey Post <corey.post0120@gmail.com>
2020-08-23 14:01:23 -0600

    updated burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,39 @@
 //connections
 let express = require("express");
-let burger = require("../models/burger.js");
 let router = express.Router();
+let burger = require("../models/burger.js");
 
-//Code for the controller
+//get route to the index
+router.get("/", function(req, res){
+    burger.all(function(data){
+        let hbsObject = {
+            burgers:data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result){
+        res.render.json({id: result.insertId})
+    });
+});
+
+router.put("/api/burgers/:id", function(req,res){
+    let condition = "id = " + req.params.id;
+
+    burger.update({
+        devoured: req.body.devoured
+    },condition, function(result){
+        if (result.changedRows == 0){
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } 
+         else{
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
