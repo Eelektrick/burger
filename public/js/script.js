9140840747d3beb9e45cda9e82b187f253dfb07c
Corey Post <corey.post0120@gmail.com>
2021-06-19 11:52:51 -0600

    updated javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,53 +1,53 @@
 $(function() {
+
+  //clear devoured burgers list
+  $("#reset-button").on('click', function(){
+    //reset form
+    document.getElementById('#devoured').reset();
+  });
     
-    $(".devour").on("click", function(event) {
+  $(".devour").on("click", function(event) {
+    event.preventDefault();
+
+    let burger_id = $(this).data("id");
+
+    let beenDevoured = {
+        devoured: true
+    };
+
+    $.ajax("/api/burgers/" + burger_id,{
+      type: "PUT",
+      data: beenDevoured
+    })
+    .then(function(){
+
+      console.log("Burger has been eaten", beenDevoured);
+      // reload page to display devoured burger in proper column
+      location.reload();
+    }); 
+  });
+
+  //This will create the post  
+  $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      let burger_id = $(this).data("id");
-
-      let beenDevoured = {
-          devoured: true
+      let newBurger = {
+          burger_name: $("#enter-text").val().trim(),
+          devoured: 0
       };
-
-      $.ajax("/api/burgers/" + burger_id,{
-        type: "PUT",
-        data: beenDevoured
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
       })
-      .then(function(){
-
-        console.log("Burger has been eaten", beenDevoured);
-        // reload page to display devoured burger in proper column
-        location.reload();
-      }); 
-    });
-
-    //This will create the post  
-    $(".create-form").on("submit", function(event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-    
-        let newBurger = {
-            burger_name: $("#enter-text").val().trim(),
-            devoured: 0
-        };
-    
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-          type: "POST",
-          data: newBurger
-        })
-        .then(
-            function() {
-                console.log("created new Burger");
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
-});
-
-//clear devoured burgers list
-$(document).on('click', "#reset-button", function(){
-  //reset form
-  document.getElementById('#devoured').reset();
+      .then(
+          function() {
+              console.log("created new Burger");
+              // Reload the page to get the updated list
+              location.reload();
+          }
+      );
+  });
 });
